0f6ee5256992d0873818367b0cf0cbe833aa2670
Christopher Carter <christopher.carter.777@gmail.com>
2018-03-23 04:40:06 -0700

    Add project files.

---
+++ SpecialTopics/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpecialTopics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
